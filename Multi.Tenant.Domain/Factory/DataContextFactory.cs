0092f101f586e66b1a51d28abccd314e0a69dd92
Ahmar Husain <ahmar.husain1@gmail.com>
2019-03-31 10:31:00 +0530

    Add files via upload

---
+++ Multi.Tenant.Domain/Factory/DataContextFactory.cs
@@ -0,0 +1,70 @@
+﻿using Multi.Tenant.Domain.Config;
+using Multi.Tenant.Domain.Factory;
+using Multi.Tenant.Domain.OLTP;
+using System;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Multi.Tenanat.App.Factory
+{
+
+    public class DataContextFactory: IDataContextFactory,IDisposable
+    {
+        private bool _disposed;
+        public OLTPContext GetDbContext(int engagementid)
+        {
+            string constring = this.GetConnectionString(engagementid);
+            return new OLTPContext(constring);
+        }
+
+        private string GetConnectionString(int engagementid)
+        {
+            string efCon = "metadata=res://*/OLTP.OLTPModel.csdl|res://*/OLTP.OLTPModel.ssdl|res://*/OLTP.OLTPModel.msl;provider=System.Data.SqlClient;";
+            using (CONFIGDBContext cob = new CONFIGDBContext())
+            {
+                Engagement engagement = cob.Engagements.Where(x => x.Id == engagementid).Select(x => x).FirstOrDefault();
+                var dataSource = engagement.EngagementDBServerName;
+                var initialCatalog = engagement.DBName;
+                var conUsername = engagement.EngagementDBLogin;
+                var conUserPWD = "S!wan@2461511809";
+                var EngDBConnTimeout = 300;
+                SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder
+                {
+                    DataSource = dataSource,
+                    InitialCatalog = initialCatalog,
+                    MultipleActiveResultSets = true,
+                    IntegratedSecurity = false,
+                    UserID = conUsername,
+                    Password = conUserPWD,
+                    ConnectTimeout = EngDBConnTimeout
+                };
+                EntityConnectionStringBuilder builder = new EntityConnectionStringBuilder
+                {
+                    Provider = "System.Data.SqlClient",
+                    ProviderConnectionString = sqlBuilder.ConnectionString
+                };
+
+                return efCon + builder.ConnectionString;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                   // this._unitOfWork.Dispose();
+                }
+            }
+            this._disposed = true;
+        }
+    }
+}
---
+++ Multi.Tenant.Domain/Factory/IDataContextFactory.cs
@@ -0,0 +1,10 @@
+﻿using Multi.Tenant.Domain.OLTP;
+using System;
+
+namespace Multi.Tenant.Domain.Factory
+{
+    public interface IDataContextFactory: IDisposable
+    {
+        OLTPContext GetDbContext(int engagementid);
+    }
+}
