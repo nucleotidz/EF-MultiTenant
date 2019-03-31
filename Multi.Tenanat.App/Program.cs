7737ace6102ba5d6a0a0825f0477a9c0a45b144b
Ahmar Husain <ahmar.husain1@gmail.com>
2019-03-31 10:27:44 +0530

    Add files via upload

---
+++ Multi.Tenanat.App/Program.cs
@@ -0,0 +1,36 @@
+﻿using Multi.Tenant.Domain.OLTP;
+using Multi.Tenant.UnitOfWork.Classes;
+using Multi.Tenant.UnitOfWork.Interfaces;
+using System;
+using System.Linq;
+
+namespace Multi.Tenanat.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Enagement id");
+            int eng = Convert.ToInt32(Console.ReadLine());
+            using (IUnitOfWorkFactory fac = new UnitOfWorkFactory())
+            {
+                IUnitOfWork uow = fac.GetUnitOfWork(eng);
+                IRepository<User> user = uow.GetRepository<User>();
+                //User u = new User
+                //{
+                //    Email = "random",
+                //    FirstName = "Random",
+                //    LastName = "Random",
+                //    CreatedBy = "Random",
+                //    CreatedDate = DateTime.Now,
+                //    ModifiedBy = "Random",
+                //    ModifiedDate = DateTime.Now,
+                //};
+                //user.Insert(u);
+                // uow.Commit();
+                var k = user.Query<User>().Select(x => x).ToList();
+
+            }
+        }
+    }
+}
